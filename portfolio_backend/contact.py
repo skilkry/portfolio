039b6c52c54f43ef04cb7f3fbd645e5899b793c8
Skilkry <skilkry@Mini-de-Skilkry.technicolor.net>
2025-07-03 22:18:52 +0200

    port

--- portfolio_backend/contact.py
+++ portfolio_backend/contact.py
@@ -53,8 +53,3 @@ def contact():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
     app.run(debug=True, host='0.0.0.0', port=port)
-
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=port)
